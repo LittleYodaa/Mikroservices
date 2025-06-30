470ab0576ec31bf40d771472a0816ab492bffc35
Patryk Kawula <patryk.kawula@programisci.eu>
2025-06-30 20:03:27 +0200

    e-commerce-app - implement product microservice entities and configure properties

---
+++ e-commerce-app/services/product/src/main/java/pl/patrykkawula/ecommerce/product/Product.java
@@ -0,0 +1,27 @@
+package pl.patrykkawula.ecommerce.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+import pl.patrykkawula.ecommerce.category.Category;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @Generated
+    private int id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
